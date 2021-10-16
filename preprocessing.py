cb5f3cd71f659e17f7afe5b5be4d11647ab8a5dc
rishiphatak <rishi.phatak1@gmail.com>
2021-10-16 16:47:51 -0500

    Don't stem text

--- preprocessing.py
+++ preprocessing.py
@@ -5,7 +5,7 @@ Created on Sat Oct 16 14:07:12 2021
 @author: Rishi2
 """
 
-from gensim.parsing.preprocessing import preprocess_documents
+from gensim.parsing.preprocessing import *
 from bs4 import BeautifulSoup
 
 MIN_LEN = 300
@@ -17,14 +17,15 @@ def remove_short_sentences(paras):
             result.append(p.get_text())
     return result
 
+# file_name : fully qualified path
 def get_key_words(file_name):
-    infile = open("docs/{}".format(file_name), encoding="utf8")
+    infile = open(file_name, encoding="utf8")
     contents = infile.read()
     soup = BeautifulSoup(contents, 'xml')
     ps = soup.find_all('P')
-    kw_mat = preprocess_documents(remove_short_sentences(ps))
-    res = []
-    for kws in kw_mat:
-        for word in kws:
-            res.append(word)
-    return res
+    full_doc = " ".join(remove_short_sentences(ps))
+    CUSTOM_FILTERS = [lambda x: x.lower(), strip_tags, strip_punctuation, strip_multiple_whitespaces,
+                   strip_numeric, remove_stopwords, strip_short, strip_non_alphanum]
+    return preprocess_string(full_doc, CUSTOM_FILTERS)
+
+# print(get_key_words("C:\Junior Year\TAMUDatathon2021\docs/01-10222.xml"))
